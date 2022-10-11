853eedd5cba0b1d1c912faf50cefd395ebceb4de
Rellik Jaeger <rellikjaeger@gmail.com>
2022-10-11 21:09:30 +0630

    Fixed resolution for vertical videos

--- yt.bat
+++ yt.bat
@@ -2,6 +2,6 @@
 setlocal enabledelayedexpansion
 if "%*" equ "" (goto :eof)
 if not exist "%userprofile%\Downloads\Video\" (mkdir "%userprofile%\Downloads\Video\")
-yt-dlp -o "%%(title)s.%%(ext)s" -f "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --mtime "%*"
+yt-dlp -o "%%(title)s.%%(ext)s" -f "bestvideo[width<=1080][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --mtime "%*"
 move /y "*.mp4" "%userprofile%\Downloads\Video\">nul
 :eof
