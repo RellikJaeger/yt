17ce567f3f240047ad62fd30e31237b2d9156649
Rellik Jaeger <rellikjaeger@gmail.com>
2022-10-11 22:08:23 +0630

    yt version: v0.2.2, date: 10/11/2022 (Tue)

--- yt.bat
+++ yt.bat
@@ -3,6 +3,8 @@ setlocal enabledelayedexpansion
 
 if "%*" equ "" (goto :eof)
 
+if "%1" equ "-v" (echo yt version: v0.2.2, date: 10/11/2022 (Tue))
+
 if "%1" equ "up" (goto :upgrade)
 
 if not exist "%userprofile%\Downloads\Video\" (mkdir "%userprofile%\Downloads\Video\")
@@ -11,6 +13,6 @@ move /y "*.mp4" "%userprofile%\Downloads\Video\">nul
 goto :eof
 
 :upgrade
-powershell -c "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser; iwr -useb https://get.scoop.sh | iex; exit" && scoop install git sudo && scoop bucket add extras && scoop install python ffmpeg busybox && cd %UserProfile% && rm -rf "./yt/" && git clone -b Windows https://github.com/RellikJaeger/yt && sudo cmd /c move /y ".\yt\yt.bat" "%SystemRoot%\" && rm -rf "./yt/" && python -m pip install --upgrade pip && pip install yt-dlp
+powershell -c "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser; iwr -useb https://get.scoop.sh | iex; exit" && scoop install git sudo phantomjs && scoop bucket add extras && scoop install python ffmpeg busybox && cd %UserProfile% && rm -rf "./yt/" && git clone -b Windows https://github.com/RellikJaeger/yt && sudo cmd /c move /y ".\yt\yt.bat" "%SystemRoot%\" && rm -rf "./yt/" && python -m pip install --upgrade pip && pip install yt-dlp
 
 :eof
